1a8d1e9f22252d8a5a041cf7bf57593f52801c59
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-07-15 18:23:14 +0800

    chore: init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{vue,ts,tsx}'],
+  plugins: [
+  ],
+  theme: {
+    extend: {
+      // borderRadius: ({ theme }) => ({ ...theme('spacing') }),
+    },
+  },
+}
